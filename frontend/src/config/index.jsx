d94792b31c5fe3b2fa3689cadf1ae1b71fa1675f
komal khokale <kkhokale15@gmail.com>
2025-09-10 11:46:08 +0530

    Frontend Deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 
-export const BASE_URL = "https://linkora-linkedinclone.onrender.com/"
+export const BASE_URL = "https://linkora-linkedinclone.onrender.com/";
 
 export const clientServer = axios.create({
     baseURL: BASE_URL
